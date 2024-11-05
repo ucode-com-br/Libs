60506bef1e0dcbb5aaa8fc22d2e6e5a9f63a0380
waldyhester <{ID}+{username}@users.noreply.github.com>
2024-11-04 21:53:22 -0300

    change bsonobjectid

--- src/Mongo/Extensions.cs
+++ src/Mongo/Extensions.cs
@@ -1,106 +1,174 @@
-//using System;
-//using System.Linq;
-//using MongoDB.Driver;
-//using MongoDB.Driver.Linq;
+using System;
 
-//namespace UCode.Mongo
-//{
-//    /// <summary>
-//    /// Provides extension methods for full-text search functionality.
-//    /// </summary>
-//    public static class Extensions
-//    {
+namespace UCode.Mongo
+{
+    /// <summary>
+    /// Provides extension methods for full-text search functionality.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Determines whether the specified type is a structure (value type).
+        /// </summary>
+        /// <param name="objectId">The object identifier instance to extend.</param>
+        /// <param name="localType">The type to check for structure status.</param>
+        /// <returns>
+        /// True if the <paramref name="localType"/> is a structure; otherwise, false.
+        /// </returns>
+        public static bool IsStructure(this IObjectId objectId, Type localType) => IsStructur(localType);
 
-//        /// <summary>
-//        /// Performs a full-text search on the specified query.
-//        /// </summary>
-//        /// <typeparam name="T">The type of the projection.</typeparam>
-//        /// <param name="query">The query to match documents against.</param>
-//        /// <param name="search">The text to search for.</param>
-//        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
-//        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search)
-//        {
-//            var filter = Builders<T>.Filter.Text(search);
-//            return query.Where(_ => filter.Inject());
-//        }
+        /// <summary>
+        /// Extension method that checks if the provided local type is a structure.
+        /// </summary>
+        /// <typeparam name="TObjectId">The type of the object identifier, which must implement 
+        /// <see cref="IComparable{T}"/> and <see cref="IEquatable{T}"/>.</typeparam>
+        /// <param name="objectId">The instance of the object identifier implementing 
+        /// <see cref="IObjectId{T}"/>.</param>
+        /// <param name="localType">The type to check if it is a structure.</param>
+        /// <returns>
+        /// Returns true if the local type is a structure; otherwise, false.
+        /// </returns>
+        public static bool IsStructure<TObjectId>(this IObjectId<TObjectId> objectId, Type localType)
+                    where TObjectId : IComparable<TObjectId>, IEquatable<TObjectId> => IsStructur(localType);
 
-//        /// <summary>
-//        /// Performs a full-text search on the specified query with the specified language.
-//        /// </summary>
-//        /// <typeparam name="T">The type of the projection.</typeparam>
-//        /// <param name="query">The query to match documents against.</param>
-//        /// <param name="search">The text to search for.</param>
-//        /// <param name="language">The language of the text to search for.</param>
-//        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
-//        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search, string language)
-//        {
-//            var filter = Builders<T>.Filter.Text(search, language);
-//            return query.Where(_ => filter.Inject());
-//        }
+        /// <summary>
+        /// Determines if a type is a structure.
+        /// </summary>
+        /// <param name="localType">The type to check.</param>
+        /// <returns>True if the type is a structure, false otherwise.</returns>
+        public static bool IsStructure<TObjectId, TUser>(this IObjectId<TObjectId, TUser> objectId, Type localType)
+            where TUser : notnull
+            where TObjectId : IComparable<TObjectId>, IEquatable<TObjectId> => IsStructur(localType);
 
-//        /// <summary>
-//        /// Performs a full-text search on the specified query with the specified options.
-//        /// </summary>
-//        /// <typeparam name="T">The type of the projection.</typeparam>
-//        /// <param name="query">The query to match documents against.</param>
-//        /// <param name="search">The text to search for.</param>
-//        /// <param name="textSearchOptions">Options for the full-text search.</param>
-//        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
-//        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search, TextSearchOptions textSearchOptions)
-//        {
-//            var filter = Builders<T>.Filter.Text(search, textSearchOptions);
-//            return query.Where(_ => filter.Inject());
-//        }
+        /// <summary>
+        /// Determines whether the specified type is a structure (a value type that is not a primitive type).
+        /// </summary>
+        /// <param name="localType">The type to be evaluated for structure characteristics.</param>
+        /// <returns>
+        /// Returns <c>true</c> if the specified type is a value type and not a primitive type, <c>false</c> otherwise.
+        /// </returns>
+        private static bool IsStructur(Type localType)
+        {
+            var result = false;
 
-//        /// <summary>
-//        /// Performs a full-text search on the specified query.
-//        /// </summary>
-//        /// <typeparam name="T">The type of the projection.</typeparam>
-//        /// <param name="query">The query to match documents against.</param>
-//        /// <param name="search">The text to search for.</param>
-//        /// <returns>An enumerable of documents matching the specified query.</returns>
-//        /// <exception cref="NotImplementedException">Thrown if the query is not of type IMongoQueryable.</exception>
-//        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> query, string search)
-//        {
-//            // If the query is of type IMongoQueryable, perform a full-text search on the specified query.
-//            if (query is IMongoQueryable<T>)
-//            {
-//                var filter = Builders<T>.Filter.Text(search);
-//                return query.Where(_ => filter.Inject());
-//            }
+            if (localType.IsValueType)
+            {
+                // Is a value type
+                if (!localType.IsPrimitive)
+                {
+                    /* Is not primitive. Remember that primitives are:
+                    Boolean, Byte, SByte, Int16, UInt16, Int32, UInt32,
+                    Int64, UInt64, IntPtr, UIntPtr, Char, Double, Single.
+                    This way, could still be Decimal, Date or Enum. */
+                    if (localType != typeof(decimal))
+                    {
+                        //Is not Decimal
+                        if (localType != typeof(DateTime))
+                        {
+                            //Is not Date
+                            if (!localType.IsEnum)
+                            {
+                                //Is not Enum. Consequently it is a structure.
+                                result = true;
+                            }
+                        }
+                    }
+                }
+            }
 
-//            // Otherwise, throw a NotImplementedException.
-//            else
-//            {
-//                throw new NotImplementedException();
-//            }
-//        }
+            return result;
+        }
 
-//        /// <summary>
-//        /// Performs a full-text search on the specified query using the specified text search options.
-//        /// </summary>
-//        /// <typeparam name="T">The type of the projection.</typeparam>
-//        /// <param name="query">The query to match documents against.</param>
-//        /// <param name="search">The text to search for.</param>
-//        /// <param name="textSearchOptions">The options for the full-text search.</param>
-//        /// <returns>An enumerable of documents matching the specified query.</returns>
-//        /// <exception cref="NotImplementedException">Thrown if the query is not of type IMongoQueryable.</exception>
-//        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> query, string search, Options.FullTextSearchOptions<T> textSearchOptions)
-//        {
-//            // Check if the query is of type IMongoQueryable
-//            if (query is IMongoQueryable<T>)
-//            {
-//                // Create a filter for the full-text search
-//                var filter = Builders<T>.Filter.Text(search, textSearchOptions);
+        //        /// <summary>
+        //        /// Performs a full-text search on the specified query.
+        //        /// </summary>
+        //        /// <typeparam name="T">The type of the projection.</typeparam>
+        //        /// <param name="query">The query to match documents against.</param>
+        //        /// <param name="search">The text to search for.</param>
+        //        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
+        //        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search)
+        //        {
+        //            var filter = Builders<T>.Filter.Text(search);
+        //            return query.Where(_ => filter.Inject());
+        //        }
 
-//                // Apply the filter to the query and return the result
-//                return query.Where(_ => filter.Inject());
-//            }
-//            else
-//            {
-//                // Throw an exception if the query is not of type IMongoQueryable
-//                throw new NotImplementedException();
-//            }
-//        }
-//    }
-//}
+        //        /// <summary>
+        //        /// Performs a full-text search on the specified query with the specified language.
+        //        /// </summary>
+        //        /// <typeparam name="T">The type of the projection.</typeparam>
+        //        /// <param name="query">The query to match documents against.</param>
+        //        /// <param name="search">The text to search for.</param>
+        //        /// <param name="language">The language of the text to search for.</param>
+        //        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
+        //        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search, string language)
+        //        {
+        //            var filter = Builders<T>.Filter.Text(search, language);
+        //            return query.Where(_ => filter.Inject());
+        //        }
+
+        //        /// <summary>
+        //        /// Performs a full-text search on the specified query with the specified options.
+        //        /// </summary>
+        //        /// <typeparam name="T">The type of the projection.</typeparam>
+        //        /// <param name="query">The query to match documents against.</param>
+        //        /// <param name="search">The text to search for.</param>
+        //        /// <param name="textSearchOptions">Options for the full-text search.</param>
+        //        /// <returns>An asynchronous enumerable of documents matching the specified query.</returns>
+        //        public static IMongoQueryable<T> FullTextSearch<T>(this IMongoQueryable<T> query, string search, TextSearchOptions textSearchOptions)
+        //        {
+        //            var filter = Builders<T>.Filter.Text(search, textSearchOptions);
+        //            return query.Where(_ => filter.Inject());
+        //        }
+
+        //        /// <summary>
+        //        /// Performs a full-text search on the specified query.
+        //        /// </summary>
+        //        /// <typeparam name="T">The type of the projection.</typeparam>
+        //        /// <param name="query">The query to match documents against.</param>
+        //        /// <param name="search">The text to search for.</param>
+        //        /// <returns>An enumerable of documents matching the specified query.</returns>
+        //        /// <exception cref="NotImplementedException">Thrown if the query is not of type IMongoQueryable.</exception>
+        //        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> query, string search)
+        //        {
+        //            // If the query is of type IMongoQueryable, perform a full-text search on the specified query.
+        //            if (query is IMongoQueryable<T>)
+        //            {
+        //                var filter = Builders<T>.Filter.Text(search);
+        //                return query.Where(_ => filter.Inject());
+        //            }
+
+        //            // Otherwise, throw a NotImplementedException.
+        //            else
+        //            {
+        //                throw new NotImplementedException();
+        //            }
+        //        }
+
+        //        /// <summary>
+        //        /// Performs a full-text search on the specified query using the specified text search options.
+        //        /// </summary>
+        //        /// <typeparam name="T">The type of the projection.</typeparam>
+        //        /// <param name="query">The query to match documents against.</param>
+        //        /// <param name="search">The text to search for.</param>
+        //        /// <param name="textSearchOptions">The options for the full-text search.</param>
+        //        /// <returns>An enumerable of documents matching the specified query.</returns>
+        //        /// <exception cref="NotImplementedException">Thrown if the query is not of type IMongoQueryable.</exception>
+        //        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> query, string search, Options.FullTextSearchOptions<T> textSearchOptions)
+        //        {
+        //            // Check if the query is of type IMongoQueryable
+        //            if (query is IMongoQueryable<T>)
+        //            {
+        //                // Create a filter for the full-text search
+        //                var filter = Builders<T>.Filter.Text(search, textSearchOptions);
+
+        //                // Apply the filter to the query and return the result
+        //                return query.Where(_ => filter.Inject());
+        //            }
+        //            else
+        //            {
+        //                // Throw an exception if the query is not of type IMongoQueryable
+        //                throw new NotImplementedException();
+        //            }
+        //        }
+    }
+}
--- src/Mongo/IObjectId.cs
+++ src/Mongo/IObjectId.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using MongoDB.Bson.IO;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
+using NPOI.Util;
+
 namespace UCode.Mongo
 {
     /// <summary>
@@ -11,5 +20,152 @@ namespace UCode.Mongo
     /// </remarks>
     public interface IObjectId : IObjectId<string>
     {
+    }
+
+    /// <summary>
+    /// Defines a generic interface for an object identifier that extends another
+    /// interface, allowing for a specific type of object ID and a string representation.
+    /// </summary>
+    /// <typeparam name="TObjectId">The type of the object identifier which must be 
+    /// comparable and equatable.</typeparam>
+    /// <remarks>
+    /// This interface can be used to enforce that types implementing it provide 
+    /// specific behaviors for comparing and equating object IDs. The requirement 
+    /// for <typeparamref name="TObjectId"/> to implement both <see cref="IComparable{T}"/> 
+    /// and <see cref="IEquatable{T}"/> ensures that users of this interface can 
+    /// perform these operations on object IDs of the specified type.
+    /// </remarks>
+    public interface IObjectId<TObjectId> : IObjectId<TObjectId, string>
+        where TObjectId : IComparable<TObjectId>, IEquatable<TObjectId>
+    {
+
+    }
+
+
+    /// <summary>
+    /// Defines an interface for objects that have a unique ID and track user creation and modification information.
+    /// </summary>
+    /// <typeparam name="TObjectId">The type of the object's ID, which must be comparable and equatable.</typeparam>
+    /// <typeparam name="TUser">The type of the user who created and updated the object.</typeparam>
+    public interface IObjectId<TObjectId, TUser>
+            where TUser : notnull
+            where TObjectId : IComparable<TObjectId>, IEquatable<TObjectId>
+    {
+        /// <summary>
+        /// Gets or sets the ID of the object.
+        /// </summary>
+        [BsonId]
+        public TObjectId Id
+        {
+            get; set;
+        }
+
+
+
+        /// <summary>
+        /// Gets or sets the user who created the object.
+        /// </summary>
+        /// <remarks>
+        /// This property is decorated with BsonElement and JsonPropertyName attributes, 
+        /// indicating how this property should be serialized/deserialized 
+        /// when working with MongoDB and JSON.
+        /// </remarks>
+        /// <typeparam name="TUser">The type of the user who created the object.</typeparam>
+        /// <returns>
+        /// The user who created the object.
+        /// </returns>
+        [BsonElement("createdBy")]
+        [JsonPropertyName("createdBy")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [BsonIgnoreIfNull]
+        public TUser? CreatedBy
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the date and time when the object was created.
+        /// </summary>
+        /// <remarks>
+        /// This property is decorated with both BsonElement and JsonPropertyName attributes
+        /// to ensure compatibility with BSON (used in MongoDB) and JSON serialization/deserialization, respectively.
+        /// </remarks>
+        /// <returns>
+        /// A DateTime value representing the creation timestamp.
+        /// </returns>
+        [BsonElement("createdAt")]
+        [JsonPropertyName("createdAt")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [BsonIgnoreIfNull]
+        public DateTime? CreatedAt
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Represents the user who updated the entity.
+        /// </summary>
+        /// <remarks>
+        /// This property is decorated with attributes for serialization to BSON and JSON formats.
+        /// </remarks>
+        /// <typeparam name="TUser">
+        /// The type of the user who performed the update. It can be any class that conforms to the expected user representation.
+        /// </typeparam>
+        /// <returns>
+        /// An instance of <typeparamref name="TUser"/> representing the user who updated the entity.
+        /// </returns>
+        [BsonElement("updatedBy")]
+        [JsonPropertyName("updatedBy")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [BsonIgnoreIfNull]
+        public TUser? UpdatedBy
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Represents the date and time when the entity was last updated.
+        /// </summary>
+        /// <remarks>
+        /// The UpdatedAt property is mapped to the "updatedAt" field in BSON and JSON formats.
+        /// It facilitates data interchange between the database and the application.
+        /// </remarks>
+        /// <returns>
+        /// A DateTime value representing the last update timestamp.
+        /// </returns>
+        [BsonElement("updatedAt")]
+        [JsonPropertyName("updatedAt")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [BsonIgnoreIfNull]
+        public DateTime? UpdatedAt
+        {
+            get;
+            set;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Represents the extra BSON elements associated with this object.
+        /// </summary>
+        [BsonExtraElements]
+        [BsonElement("extraElements")]
+        [BsonDictionaryOptions(DictionaryRepresentation.Document)]
+        [BsonIgnoreIfNull]
+        [JsonExtensionData]
+        [JsonPropertyName("extraElements")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Dictionary<string, object?>? ExtraElements
+        {
+            get; set;
+        }
+
+
     }
 }
