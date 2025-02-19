27e9e3e7ae6a8b3ed6b74e9a8b13956e59bc4eb8
Syllas de Souza <waldyhester@gmail.com>
2025-02-19 17:02:23 -0300

    .

--- src/Mongo/Serializers/IgnorableDataSerializer.cs
+++ src/Mongo/Serializers/IgnorableDataSerializer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using MongoDB.Bson.IO;
@@ -11,24 +12,54 @@ namespace UCode.Mongo.Serializers
     /// <summary>
     /// A generic BSON serializer that ignores properties or fields decorated with
     /// <see cref="IgnorableDataAttribute"/> when the object being serialized is not the root document.
+    /// This serializer also implements <see cref="IBsonDocumentSerializer"/>, so that member metadata is available
+    /// (i.e. members are represented as fields).
     /// </summary>
     /// <typeparam name="T">The type to serialize.</typeparam>
     /// <remarks>
     /// <para>
-    /// When serializing an object of type <typeparamref name="T"/>, any public property or field
-    /// marked with <see cref="IgnorableDataAttribute"/> will be omitted if the object is nested (i.e. not the root).
-    /// For example, consider a <c>Company</c> class where properties such as <c>Value</c> and <c>City</c> are marked
-    /// with <c>[IgnorableData]</c>. When a <c>Company</c> object is embedded inside another object (e.g., as an element
-    /// of an array in a <c>Person</c> document), those properties will be excluded from the BSON output.
+    /// When serializing an object of type <typeparamref name="T"/>, any public property or field marked with 
+    /// <see cref="IgnorableDataAttribute"/> will be omitted if the object is nested (i.e. not the root document).
+    /// For example, if a <c>Company</c> class has properties <c>Value</c> and <c>City</c> marked with <c>[IgnorableData]</c>,
+    /// those members will be omitted when a <c>Company</c> is embedded inside another object, but they will be included if the
+    /// <c>Company</c> object is the root.
     /// </para>
     /// <para>
-    /// **Exceptions:**  
-    /// If the <see cref="BsonSerializationContext.Writer"/> cannot be cast to a <see cref="BsonWriter"/>, an
-    /// <see cref="InvalidOperationException"/> is thrown.
+    /// <b>Exceptions:</b>  
+    /// An <see cref="InvalidOperationException"/> is thrown if the underlying writer cannot be cast to a <see cref="BsonWriter"/>.
     /// </para>
     /// </remarks>
-    public class IgnorableDataSerializer<T> : SerializerBase<T>
+    public class IgnorableDataSerializer<T> : SerializerBase<T>, IBsonDocumentSerializer
     {
+        // A static dictionary that maps member names to BsonSerializationInfo.
+        // This metadata is used to support IBsonDocumentSerializer.
+        private static readonly Dictionary<string, BsonSerializationInfo> __memberSerializationInfo;
+
+        static IgnorableDataSerializer()
+        {
+            __memberSerializationInfo = new Dictionary<string, BsonSerializationInfo>();
+
+            // Process public properties.
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                      .Where(p => p.CanRead && p.GetIndexParameters().Length == 0);
+            foreach (var prop in properties)
+            {
+                var serializer = BsonSerializer.LookupSerializer(prop.PropertyType);
+                // Create metadata for the property. Even if the property is marked with IgnorableData,
+                // it must be included in the mapping.
+                __memberSerializationInfo[prop.Name] =
+                    new BsonSerializationInfo(prop.Name, serializer, prop.PropertyType);
+            }
+
+            // Process public fields.
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                var serializer = BsonSerializer.LookupSerializer(field.FieldType);
+                __memberSerializationInfo[field.Name] =
+                    new BsonSerializationInfo(field.Name, serializer, field.FieldType);
+            }
+        }
 
         /// <summary>
         /// Serializes an instance of <typeparamref name="T"/> to BSON.
@@ -41,29 +72,22 @@ namespace UCode.Mongo.Serializers
         /// </exception>
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, T value)
         {
-            // Write BSON null if the object is null.
             if (value == null)
             {
                 context.Writer.WriteNull();
                 return;
             }
 
-            var type = typeof(T);
-
-            // Cast the writer to BsonWriter to access the SerializationDepth property.
-            // (e.g. BsonBinaryWriter and BsonJsonWriter derive from BsonWriter.)
-            var bsonWriter = context.Writer as BsonWriter ?? throw new InvalidOperationException("Expected writer to be a BsonWriter.");
-
+            // Cast the writer to BsonWriter to access SerializationDepth.
+            var bsonWriter = context.Writer as BsonWriter
+                             ?? throw new InvalidOperationException("Expected writer to be a BsonWriter.");
             // When the object is being serialized as the root document, SerializationDepth == 0.
-            var isRoot = bsonWriter.SerializationDepth == 0;
+            bool isRoot = bsonWriter.SerializationDepth == 0;
 
             bsonWriter.WriteStartDocument();
 
-            // Serialize properties.
-            ProcessProperties(context, value, bsonWriter, isRoot, type);
-
-            // Serialize fields.
-            ProcessFields(context, value, bsonWriter, isRoot, type);
+            ProcessProperties(context, value, bsonWriter, isRoot, typeof(T));
+            ProcessFields(context, value, bsonWriter, isRoot, typeof(T));
 
             bsonWriter.WriteEndDocument();
         }
@@ -80,24 +104,15 @@ namespace UCode.Mongo.Serializers
         {
             var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
                                  .Where(p => p.CanRead && p.GetIndexParameters().Length == 0);
-
             foreach (var prop in properties)
             {
-                // Skip properties marked with [IgnorableData] if the object is not the root.
+                // If the property is decorated with IgnorableDataAttribute and the object is not the root, skip it.
                 if (!isRoot && prop.IsDefined(typeof(IgnorableDataAttribute), inherit: true))
-                {
                     continue;
-                }
 
                 bsonWriter.WriteName(prop.Name);
-
-                // Retrieve the property value.
                 var propValue = prop.GetValue(value);
-
-                // Look up the appropriate serializer for the property’s type.
                 var serializer = BsonSerializer.LookupSerializer(propValue?.GetType() ?? typeof(object));
-
-
                 serializer.Serialize(context, propValue);
             }
         }
@@ -115,11 +130,8 @@ namespace UCode.Mongo.Serializers
             var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
             foreach (var field in fields)
             {
-                // Skip fields marked with [IgnorableData] if the object is not the root.
                 if (!isRoot && field.IsDefined(typeof(IgnorableDataAttribute), inherit: true))
-                {
                     continue;
-                }
 
                 bsonWriter.WriteName(field.Name);
                 var fieldValue = field.GetValue(value);
@@ -139,7 +151,31 @@ namespace UCode.Mongo.Serializers
         /// </remarks>
         public override T Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) =>
             BsonSerializer.Deserialize<T>(context.Reader);
+
+        #region IBsonDocumentSerializer Members
+
+        /// <summary>
+        /// Attempts to get the member serialization information for a given member name.
+        /// </summary>
+        /// <param name="memberName">The name of the member.</param>
+        /// <param name="serializationInfo">
+        /// When this method returns, contains the <see cref="BsonSerializationInfo"/> for the specified member,
+        /// if found; otherwise, <c>null</c>.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the member was found; otherwise, <c>false</c>.
+        /// </returns>
+        public bool TryGetMemberSerializationInfo(string memberName, out BsonSerializationInfo serializationInfo) => __memberSerializationInfo.TryGetValue(memberName, out serializationInfo);
+
+        /// <summary>
+        /// Gets all member names represented by this serializer.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerable{String}"/> containing all member names.</returns>
+        public IEnumerable<string> GetMemberNames() => __memberSerializationInfo.Keys;
+
+        #endregion
     }
+
 
 
 }
